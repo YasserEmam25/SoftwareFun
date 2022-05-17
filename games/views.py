a792de520296f17f557804f28a32d4926577d329
Yasser Emam <YasserEmam2002@gmail.com>
2022-05-17 21:21:16 +0200

    finished getting data from database, but images doesn't work till now

--- games/views.py
+++ games/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
-
+from .models import Game
+from SoftwareFun import settings
 # Create your views here.
 
 
 def index(request):
 
-    games = []
+    games = Game.objects.all()
 
-    return render(request, "index.html", {'games': games})
+    games1 = games[0:3]
+    games2 = games[3: 6]
+    games3 = games[6:9]
+
+    for game in games3:
+        print(game)
+
+    return render(request, "index.html",
+                  {'all_games': games, 'media_url': settings.MEDIA_URL})
