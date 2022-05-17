73688e877a36a764ccf4a6de395025eb444bcdd8
Yasser Emam <YasserEmam2002@gmail.com>
2022-05-17 21:46:48 +0200

    finished the backend I guess

--- SoftwareFun/urls.py
+++ SoftwareFun/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls.static import static
 from django.conf import settings
-# from djanog.conf.urls.static import static
+
 
 from games.views import index
 
@@ -25,5 +26,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 ]
 
-# urlpatterns = urlpatterns + \
-# static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns = urlpatterns + \
+    static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
